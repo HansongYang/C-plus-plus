8939f5633b0e57df306c7c10ac5335d263709101
MegaMind1000 <yanghansong1996@live.com>
2017-01-12 20:29:49 -0500

    Chemistry molecules names

--- main.cpp
+++ main.cpp
@@ -2,39 +2,108 @@
 
 using namespace std;
 
-void numbers(int a){
-    int num = 1;
-    int original = 1;
-
-    for(int i = 0; i < a; i++){
-        for(int j = 0; j < (2*a - 1); j++){
-            if((j + num) == a && j < a){
-                cout<<num;
-                if(num != 1){
-                num--;
-                }
-                else{
-                    num++;
-                }
-            }
-            else if(j >= a && num <= i + 1){
-                cout<<num;
-                num++;
-            }
-            else{
-                cout<<" ";
-            }
-
-        }
-            cout<<endl;
-            original++;
-            num = original;
-        }
-    }
-
-
 int main()
 {
-    numbers(9);
+    std::string A;
+    std::string B;
+    int b;
+    cout<<"Please enter the two symbols of element and the atoms of the second element, thank you."<<endl;
+
+    cin>>A;
+    cin>>B;
+    cin>>b;
+
+    cout<< "The geometrical shape of one "<< A <<" atom surrouded by "<< b <<" "<< B <<" atoms is ";
+
+    int v = 0;
+
+    if(B == "Be"){
+        v = 3;
+    }
+    else if(B == "C"){
+        v = 4;
+    }
+     else if(B == "Si"){
+        v = 4;
+    }
+     else if(B == "N"){
+        v = 5;
+    }
+     else if(B == "P"){
+        v = 5;
+    }
+     else if(B == "As"){
+        v = 5;
+    }
+     else if(B == "O"){
+        v = 6;
+    }
+     else if(B == "S"){
+        v = 6;
+    }
+     else if(B == "Se"){
+        v = 6;
+    }
+     else if(B == "F"){
+        v = 7;
+    }
+     else if(B == "Cl"){
+        v = 7;
+    }
+     else if(B == "Br"){
+        v = 7;
+    }
+     else if(B == "I"){
+        v = 7;
+    }
+     else if(B == "Xe"){
+        v = 8;
+    }
+
+    int e = v - b;
+    int n = e/2;
+
+    if(b == 2 && n == 0){
+        cout<< "liner."<<endl;
+    }
+    else if(b == 2 && n == 1){
+        cout<<"bent."<<endl;
+    }
+    else if(b == 2 && n == 2){
+        cout<<"bent."<<endl;
+    }
+    else if(b == 2 && n == 3){
+        cout<<"linear."<<endl;
+    }
+    else if(b == 3 && n == 0){
+        cout<<"trigonal planar."<<endl;
+    }
+    else if(b == 3 && n == 1){
+        cout<<"trigonal pyramidal."<<endl;
+    }
+    else if(b == 3 && n == 2){
+        cout<<"T-shaped."<<endl;
+    }
+    else if(b == 4 && n == 0){
+        cout<<"tetrahedral."<<endl;
+    }
+    else if(b == 4 && n == 1){
+        cout<<"seesaw."<<endl;
+    }
+    else if(b == 4 && n == 2){
+        cout<<"square planar."<<endl;
+    }
+    else if(b == 5 && n == 0){
+        cout<<"trigonal bipyramidal."<<endl;
+    }
+    else if(b == 5 && n == 1){
+        cout<<"square pyramidal."<<endl;
+    }
+    else if(b == 6 && n == 0){
+        cout<<"octahedral."<<endl;
+    }
+    else{
+        cout<<"unknown."<<endl;
+    }
     return 0;
 }
